7fa610987504392a272468cb45901d2dc35171f2
Eric Kim <bosung90@gmail.com>
2016-02-12 21:11:56 -0800

    adding Post

---
+++ components/pages/Post.js
@@ -0,0 +1,111 @@
+import React, {
+	Component,
+	View,
+	Text,
+	Image,
+	ScrollView,
+	StyleSheet,
+	Dimensions,
+} from 'react-native'
+const {height, width} = Dimensions.get('window')
+
+export default class Post extends Component {
+	componentDidMount(){
+		// alert(width)
+	}
+	render() {
+		return(
+			<View>
+				<ScrollView style={{height: height}}>
+					<View style={{backgroundColor: 'red'}}>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+						<Text>Hello Post Page</Text>
+					</View>
+				</ScrollView>
+			</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		// flex:1,
+		// flexDirection: 'column',
+		// height: 2000,
+	},
+
+})
